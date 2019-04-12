d40fbe39c0c29300bb5cada40f17a44a4a6b464b
Shou Gondo <shou@miepc.local>
2019-04-12 23:04:36 +0900

    Render json

--- cakephp-fundamental/mycakeapp/src/Controller/HelloController.php
+++ cakephp-fundamental/mycakeapp/src/Controller/HelloController.php
@@ -6,18 +6,17 @@ use App\Controller\AppController;
 class HelloController extends AppController {
     public $autoRender = false;
 
+    private $data = [
+        ['name' => 'taro', 'mail' => 'taro@yamada', 'tel' => '090-999-999'],
+        ['name' => 'hanako', 'mail' => 'hanako@flower', 'tel' => '080-888-888'],
+        ['name' => 'sachiko', 'mail' => 'sachico@happy', 'tel' => '070-777-777']
+    ];
+
     public function index() {
-        $id = 'no name';
+        $id = 0;
         if (isset($this->request->query['id'])) {
             $id = $this->request->query['id'];
         }
-        $pass = 'no password';
-        if (isset($this->request->query['pass'])) {
-            $pass = $this->request->query['pass'];
-        }
-        echo '<html><body><h1>Hello!</h1>';
-        echo '<ul><li>your id: ' . $id . '</li>';
-        echo '<li>password: ' . $pass . '</li>';
-        echo '</body></html>';
+        echo json_encode($this->data[$id]);
     }
 }
