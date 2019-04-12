d29baf81bede3d0aee645aab29e032a7b8644758
Shou Gondo <shou@miepc.local>
2019-04-12 22:59:57 +0900

    Avoid no params warning

--- cakephp-fundamental/mycakeapp/src/Controller/HelloController.php
+++ cakephp-fundamental/mycakeapp/src/Controller/HelloController.php
@@ -7,8 +7,14 @@ class HelloController extends AppController {
     public $autoRender = false;
 
     public function index() {
-        $id = $this->request->query['id'];
-        $pass = $this->request->query['pass'];
+        $id = 'no name';
+        if (isset($this->request->query['id'])) {
+            $id = $this->request->query['id'];
+        }
+        $pass = 'no password';
+        if (isset($this->request->query['pass'])) {
+            $pass = $this->request->query['pass'];
+        }
         echo '<html><body><h1>Hello!</h1>';
         echo '<ul><li>your id: ' . $id . '</li>';
         echo '<li>password: ' . $pass . '</li>';
