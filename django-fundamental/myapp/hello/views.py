73421af059b77882d9058a29d8ffc0fbce7cc0fc
Shou Takenaka <sasaminn@gmail.com>
2019-04-28 13:39:55 +0000

    Render with html template

--- django-fundamental/myapp/hello/views.py
+++ django-fundamental/myapp/hello/views.py
@@ -2,6 +2,5 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 # Create your views here.
-def index(request, nickname, age):
-    result = 'your account: ' + nickname + ' (' + str(age) + ').'
-    return HttpResponse(result)
+def index(request):
+    return render(request, 'hello/index.html')
