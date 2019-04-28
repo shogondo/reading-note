8788d191762cd5ba380f8525d0f4d8c52c2f5cc5
Shou Takenaka <sasaminn@gmail.com>
2019-04-28 13:51:58 +0000

    Add page transition

--- django-fundamental/myapp/hello/views.py
+++ django-fundamental/myapp/hello/views.py
@@ -5,6 +5,15 @@ from django.http import HttpResponse
 def index(request):
     params = {
         'title': 'Hello/Index',
-        'msg': 'これは、サンプルで作ったページです。'
+        'msg': 'これは、サンプルで作ったページです。',
+        'goto': 'next'
+    }
+    return render(request, 'hello/index.html', params)
+
+def next(request):
+    params = {
+        'title': 'Hello/Next',
+        'msg': 'これは、もう１つのページです。',
+        'goto': 'index',
     }
     return render(request, 'hello/index.html', params)
