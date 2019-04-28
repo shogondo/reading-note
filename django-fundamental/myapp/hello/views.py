d58fcae5a86e98488b491ac44458e4116eef144c
Shou Takenaka <sasaminn@gmail.com>
2019-04-28 13:24:29 +0000

    Use path parameter

--- django-fundamental/myapp/hello/views.py
+++ django-fundamental/myapp/hello/views.py
@@ -2,10 +2,6 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 # Create your views here.
-def index(request):
-    if 'msg' in request.GET:
-        msg = request.GET['msg']
-        result = 'you typed "' + msg + '".'
-    else:
-        result = 'please send msg parameter'
+def index(request, id, nickname):
+    result = 'your id:' + str(id) + ', name: "' + nickname + '".'
     return HttpResponse(result)
